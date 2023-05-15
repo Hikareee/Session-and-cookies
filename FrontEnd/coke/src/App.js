29a85917e6df4ffc987b806358f15cccca17eec7
HikariNoRyu <88818748+HikariNoRyu@users.noreply.github.com>
2023-05-15 21:52:23 +0700

    Yes i cahnged le file

--- FrontEnd/coke/src/App.js
+++ FrontEnd/coke/src/App.js
@@ -1,81 +1,45 @@
 import React, { useState } from 'react';
 import { useCookies } from 'react-cookie';
-import TextField from '@mui/material/TextField';
-import Button from '@mui/material/Button';
-import "./App.css"
 
-function App() {
+const App = () => {
+   const [name, setName] = useState('');
+   const [pwd, setPwd] = useState('');
+   const [cookies, setCookie] = useCookies(['user']);
 
-  // --------------------- CONST --------------------- //
-  const [name, setName] = useState('');
-  const [pass, setPass] = useState('');
-  const [cookies, setCookie] = useCookies(['user']);
-
-  // When submit button is clicked
-  const handle = () => {
-     setCookie('Name', name, { path: '/' }); // Sets username
-     setCookie('Password', pass, { path: '/' }); // Sets password
-     window.location.reload(); // Refreshes page
-  }
-    
-  return (
-    <div className="App">
-      <h1>Set a Cookie!</h1>
-      <h3> Please enter the following information: </h3>
-
+   const handle = () => {
+      setCookie('Name', name, { path: '/' });
+      setCookie('Password', pwd, { path: '/' });
+   };
+   return (
+      <div className="App">
+      <h1>Name of the user:</h1>
+      <input
+         placeholder="Name"
+         value={name}
+         onChange={(e) => setName(e.target.value)}
+      />
+      <h1>Password of the user:</h1>
+      <input
+         type="password"
+         placeholder="Password"
+         value={pwd}
+         onChange={(e) => setPwd(e.target.value)}
+      />
       <div>
-        <TextField 
-          id="outlined-basic"
-          label="Username"
-          variant="outlined"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-        />
+      <button onClick={handle}>Set Cookie</button>{' '}
       </div>
-
-      <br/>
-
+      <br />
+      {cookies.Name && (
       <div>
-        <TextField 
-          id="outlined-basic"
-          label="Password"
-          variant="outlined"
-          value={pass}
-          onChange={(e) => setPass(e.target.value)}
-        />
+         Name: <p>{cookies.Name}</p>
       </div>
-
-      <br/>
-
+      )}
+      {cookies.Password && (
       <div>
-        <Button
-          variant="contained"
-          onClick={handle}
-        >
-        Submit Cookie
-        </Button>
-      </div>  
-
-      <br/>
-      <br/>
-      <br/>
-
-      <div>
-        <h3>Last Set Cookie:</h3>
-        {cookies.Name && (
-          <div>
-            <p>Name: {cookies.Name}</p>
-          </div>
-        )}
-
-        {cookies.Password && (
-          <div>
-            <p>Password: {cookies.Password}</p>
-          </div>
-        )}
+         Password: <p>{cookies.Password}</p>
       </div>
-    </div>
-  );
-}
-
+      )}
+      </div>
+   );
+};
 export default App;
